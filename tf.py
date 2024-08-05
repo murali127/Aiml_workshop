6aaa2a5d79b58c871bfc584ddc36165f8bd6b738
Murali_paila <114466262+murali127@users.noreply.github.com>
2024-08-05 07:20:25 +0000

    rfg

--- tf.py
+++ tf.py
@@ -1,7 +1,7 @@
 import tensorflow as tensorflow
 import numpy as numpy
 from tensorflow import keras
-model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model = tensorflow.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)

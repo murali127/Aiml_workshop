1b8b3e65086d6216bf882ae48ddb53d113f0ddac
Murali_paila <114466262+murali127@users.noreply.github.com>
2024-08-05 07:24:18 +0000

    wdf

--- tf.py
+++ tf.py
@@ -1,9 +1,13 @@
-import tensorflow as tensorflow
-import numpy as numpy
+import tensorflow as tf
+import numpy as np
 from tensorflow import keras
-model = tensorflow.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
-xs = numpy.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = numpy.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
 model.fit(xs, ys, epochs=500)
-print(model.predict([10.0]))
+
+print(model.predict(np.array([10.0])))

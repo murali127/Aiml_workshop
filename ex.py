22f93e7c2ef93b598952093a36f28530074227da
Murali_paila <114466262+murali127@users.noreply.github.com>
2024-08-05 09:34:39 +0000

    qwedrfghj

--- ex.py
+++ ex.py
@@ -9,11 +9,11 @@ model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 # Define the training data
-xs = np.array([50.0, 100.0, 150.0, 200.0, 250.0, 300.0], dtype=float)
-ys = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0], dtype=float)
+xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
+ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5], dtype=float)
 
 # Train the model
-model.fit(xs, ys, epochs=100)
+model.fit(xs, ys, epochs=1000)
 
 # Make a prediction
 print(model.predict(np.array([7.0])))
